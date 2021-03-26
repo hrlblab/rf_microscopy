2a342f3bc4f92ed5c41398829aaa96f7b7fd950d
yilinliu610730 <liuyilin0730@foxmail.com>
2021-03-26 15:48:04 +0800

    first draft

--- python/fieldSynthesis.py
+++ python/fieldSynthesis.py
@@ -1,9 +1,15 @@
+from typing import List, Union
 import numpy as np
+import pandas as pandas
 import scipy as sci
 import scipy.fftpack as ft
 import scipy.signal as sig
 from scipy.stats import norm
 import matplotlib.pyplot as plt
+import math
+from PIL import Image
+import matplotlib.pyplot
+import pandas as pd
 
 ''' Field Synthesis
 Python-based demonstration of Field Synthesis
@@ -38,6 +44,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
+
 def createAnnulus(n=256, r=32, w=4):
     ''' createAnnulus - create a ring-like structure
     INPUT
@@ -49,25 +56,18 @@ def createAnnulus(n=256, r=32, w=4):
     '''
     if np.isscalar(n):
         v = np.arange(n)
-        v = v - np.floor(n/2)
+        v = v - np.floor(n / 2)
     else:
         v = n
 
-    y,x = np.meshgrid(v,v)
-    q = np.hypot(x,y)
-    annulus = abs(q-r) < w
-
-    #FIXME
-    #abs_v = abs(v)
-    #selected_columns = (abs_v < 198 + w / 2 & abs_v > 198 - w / 2) | ...
-    #(v < w / 2 & v > -w / 2)
-    #latticeFourierMask = annulus
-    #latticeFourierMask: ~selected_columns = False
-    #latticeFourierMask = np.double(latticeFourierMask)
+    y, x = np.meshgrid(v, v)
+    q = np.hypot(x, y)
+    annulus = abs(q - r) < w
 
     return annulus
 
-def doConventionalScan(Fsqmod,Lsqmod):
+
+def doConventionalScan(Fsqmod, Lsqmod):
     '''Simulate Conventional digital scanning / dithering
         INPUT
         F_sqmod - Square modulus of F at the front focal plane
@@ -77,14 +77,15 @@ def doConventionalScan(Fsqmod,Lsqmod):
     '''
     # Manually scan by shifting Fsqmod and multiplying by Lsqmod
     scanned = np.zeros(Fsqmod.shape)
-    center = Lsqmod.shape[1]//2
+    center = Lsqmod.shape[1] // 2
 
-    for x in range(np.size(Fsqmod,1)):
-        scanned = scanned + np.roll(Fsqmod,x-center,1)*Lsqmod[center,x]
+    for x in range(np.size(Fsqmod, 1)):
+        scanned = scanned + np.roll(Fsqmod, x - center, 1) * Lsqmod[center, x]
 
     return scanned
 
-def doConventionalScanHat(F_hat,L_hat):
+
+def doConventionalScanHat(F_hat, L_hat):
     '''Simulate Conventional digital scanning / dithering from frequency space representations
        INPUT
        F_hat - Mask at back focal plane
@@ -96,18 +97,18 @@ def doConventionalScanHat(F_hat,L_hat):
     F = ft.ifft2(F_hat)
     F = ft.fftshift(F)
     # This is the illumination intensity pattern
-    Fsqmod = np.real(F*np.conj(F))
+    Fsqmod = np.real(F * np.conj(F))
 
     L_hat = ft.ifftshift(L_hat)
     L = ft.ifft2(L_hat)
     L = ft.fftshift(L)
-    Lsqmod = L*np.conj(L)
+    Lsqmod = L * np.conj(L)
 
-    scanned = doConventionalScan(Fsqmod,Lsqmod)
+    scanned = doConventionalScan(Fsqmod, Lsqmod)
     return scanned
-    
 
-def doFieldSynthesisLineScan(F_hat,L_hat):
+
+def doFieldSynthesisLineScan(F_hat, L_hat):
     '''Simulate Field Synthesis Method
         INPUT
         F_hat - Frequency space representation of illumination pattern, mask at back focal plane
@@ -120,13 +121,14 @@ def doFieldSynthesisLineScan(F_hat,L_hat):
 
     for a in range(fieldSynthesis.shape[1]):
         # Instaneous scan in frequency space
-        T_hat_a = F_hat * np.roll(L_hat,a-fieldSynthesis.shape[1]//2,1)
+        T_hat_a = F_hat * np.roll(L_hat, a - fieldSynthesis.shape[1] // 2, 1)
         # Instaneous scan in object space
-        T_a = ft.fftshift( ft.fft2( ft.ifftshift(T_hat_a) ) )
+        T_a = ft.fftshift(ft.fft2(ft.ifftshift(T_hat_a)))
         # Incoherent summing of the intensities
-        fieldSynthesis = fieldSynthesis + np.abs(T_a)**2
+        fieldSynthesis = fieldSynthesis + np.abs(T_a) ** 2
 
     return fieldSynthesis
+
 
 def demoFieldSynthesis():
     '''Demonstrate Field Synthesis Method with Plots
@@ -136,8 +138,8 @@ def demoFieldSynthesis():
          None
     '''
     # FixMe
-    #plt.rc('text', usetex=True)
-    fig, ax = plt.subplots(2,4,sharey=True,sharex=True,figsize=(16,9))
+    # plt.rc('text', usetex=True)
+    fig, ax = plt.subplots(2, 4, sharey=True, sharex=True, figsize=(16, 9))
 
     # Create F, the illumination pattern
     F_hat = createAnnulus()
@@ -145,65 +147,137 @@ def demoFieldSynthesis():
     F = ft.ifft2(F_hat)
     F = ft.fftshift(F)
     # This is the illumination intensity pattern
-    Fsqmod = np.real(F*np.conj(F))
+    Fsqmod = np.real(F * np.conj(F))
 
-    #FIXME
-    #plt.figure()
-    #plt.title('F')
-    #plt.imshow(Fsqmod, cmap='plasma')
-    #plt.show(block=False)
-    ax[0,0].imshow(Fsqmod, cmap='plasma')
-    ax[0,0].set_title('F(x,z)')
+    # FIXME
+    # plt.figure()
+    # plt.title('F')
+    # plt.imshow(Fsqmod, cmap='plasma')
+    # plt.show(block=False)
+    ax[0, 0].imshow(Fsqmod, cmap='plasma')
+    ax[0, 0].set_title('F(x,z)')
 
     # Create L, the scan profile
     L = np.zeros_like(Fsqmod)
-    center = L.shape[1]//2
+    center = L.shape[1] // 2
     sigma = 30
-    L[center,:] = norm.pdf(np.arange(-center,center),0,sigma)
+    L[center, :] = norm.pdf(np.arange(-center, center), 0, sigma)
     # L[L.shape[1]//2,:] = 1
     # The square modulus of L is the object space
-    Lsqmod = L*np.conj(L)
+    Lsqmod = L * np.conj(L)
     # This is the line scan profile used in Field Synthesis
     L_hat = ft.fftshift(ft.fft2(ft.ifftshift(L)))
 
-    ax[0,1].imshow(L, cmap='plasma')
-    ax[0,1].set_title('$ L(x)\delta(z) $')
+    ax[0, 1].imshow(L, cmap='plasma')
+    ax[0, 1].set_title('$ L(x)\delta(z) $')
 
-    ax[0,2].imshow(Lsqmod, cmap='plasma')
-    ax[0,2].set_title('$ |L(x)\delta(z)|^2 $')
+    ax[0, 2].imshow(Lsqmod, cmap='plasma')
+    ax[0, 2].set_title('$ |L(x)\delta(z)|^2 $')
 
-    ax[0,3].imshow(np.abs(L_hat), cmap='plasma')
-    ax[0,3].set_title('$\hat{L}(k_x) $')
+    ax[0, 3].imshow(np.abs(L_hat), cmap='plasma')
+    ax[0, 3].set_title('$\hat{L}(k_x) $')
 
     # Manually scan by shifting Fsqmod and multiplying by Lsqmod
-    scanned = doConventionalScan(Fsqmod,Lsqmod)
+    scanned = doConventionalScan(Fsqmod, Lsqmod)
 
-    ax[1,0].imshow(scanned, cmap='plasma')
-    ax[1,0].set_title('Scanned: $ \sum_{x\'} |F(x\',z)|^2|L(x-x\')|^2 $')
+    ax[1, 0].imshow(scanned, cmap='plasma')
+    ax[1, 0].set_title('Scanned: $ \sum_{x\'} |F(x\',z)|^2|L(x-x\')|^2 $')
 
     # Manually scanning is a convolution operation
     # There are potentially boundary effects here
-    convolved = sig.fftconvolve(Fsqmod,Lsqmod,'same')
+    convolved = sig.fftconvolve(Fsqmod, Lsqmod, 'same')
 
-    ax[1,1].imshow(convolved, cmap='plasma')
-    ax[1,1].set_title('Convolved: $ |F(x,z)|^2 ** |L(x)\delta(z)|^2 $')
+    ax[1, 1].imshow(convolved, cmap='plasma')
+    ax[1, 1].set_title('Convolved: $ |F(x,z)|^2 ** |L(x)\delta(z)|^2 $')
 
     # This manual implementation of Fourier transform based convolution
     # actually does circular convolution
-    convolvedft = ft.fftshift(ft.fft2(ft.ifft2(ft.ifftshift(Fsqmod)) *ft.ifft2(ft.ifftshift(Lsqmod))))
+    convolvedft = ft.fftshift(ft.fft2(ft.ifft2(ft.ifftshift(Fsqmod)) * ft.ifft2(ft.ifftshift(Lsqmod))))
     convolvedft = np.real(convolvedft)
 
-    ax[1,2].imshow(convolvedft, cmap='plasma')
-    ax[1,2].set_title(r'Convolved FT: $ \mathcal{F}^{-1} \{ \mathcal{F}\{|F|^2\} \mathcal{F}\{|L(x)\delta(z)|^2\} \} $')
+    ax[1, 2].imshow(convolvedft, cmap='plasma')
+    ax[1, 2].set_title(
+        r'Convolved FT: $ \mathcal{F}^{-1} \{ \mathcal{F}\{|F|^2\} \mathcal{F}\{|L(x)\delta(z)|^2\} \} $')
 
     # Do the Field Synthesis method of performing a line scan at the back focal plane
-    fieldSynthesis = doFieldSynthesisLineScan(F_hat,L_hat)
+    fieldSynthesis = doFieldSynthesisLineScan(F_hat, L_hat)
 
-    ax[1,3].imshow(fieldSynthesis, cmap='plasma')
-    ax[1,3].set_title('Field Synthesis: $ \sum_a |\mathcal{F}^{-1}\{ \hat{F}(k_x,k_z)\hat{L}(k_x-a) \}|^2 $')
+    ax[1, 3].imshow(fieldSynthesis, cmap='plasma')
+    ax[1, 3].set_title('Field Synthesis: $ \sum_a |\mathcal{F}^{-1}\{ \hat{F}(k_x,k_z)\hat{L}(k_x-a) \}|^2 $')
 
     plt.show()
     plt.pause(0.001)
 
+
 if __name__ == "__main__":
+    n = 4096
+    r = 256
+    w = 5
+    offset = 256
+    dispRange: List[Union[int, float]] = []
+    for i in range (-600, 601):
+        dispRange.append(i + math.floor(n / 2) + 1)
+    v = []
+    for i in range(0, n):
+        v.append(i - math.floor(n / 2))
+    annulus = createAnnulus(v, r, w)
+    abs_v = np.absolute(v)
+    selected_columns = []
+    for i in range(0, n):
+        if (offset + w / 2 > abs_v[i] > offset - w / 2) or (w / 2 > v[i] > -w / 2):
+            selected_columns.append(1)
+        else:
+            selected_columns.append(0)
+    latticeFourierMask = annulus
+    for c in range(0, n):
+        if selected_columns[c]:
+            latticeFourierMask[:, c] = True
+
+    fieldSynthesisProfile = ft.fftshift(ft.ifft(ft.ifftshift(latticeFourierMask)))
+    fieldSynthesisProfile = [sum(i) for i in fieldSynthesisProfile]
+
+    lattice_efield = ft.fftshift(ft.ifft2(ft.ifftshift(latticeFourierMask)))
+    lattice = abs(lattice_efield) ** 2
+    latticeLineProfile = sum(lattice, 2)
+    latticeLineProfile = latticeLineProfile * n
+
+    lattice_hat = ft.fftshift(ft.fft2(ft.ifftshift(lattice)))
+
+    period = n / offset
+    if period == round(period):
+        latticeDithered = np.zeros(len(lattice));
+        for s in range(math.floor(-period / 2), math.floor(period / 2) - 1):
+            latticeDithered = latticeDithered + np.roll(lattice, s)
+        latticeDithered = latticeDithered / period
+    else:
+        for x in lattice_hat:
+            for y in x:
+                latticeDithered = y * (np.sinc(v / period))
+        latticeDithered = ft.fftshift(ft.ifft2(ft.ifftshift(latticeDithered)))
+
+    latticeDithered_hat = ft.fftshift(ft.fft2(ft.ifftshift(latticeDithered)))
+
+    # Plot 2x3
+    fig, ax = plt.subplots(2, 3, sharey=True, sharex=True, figsize=(16, 9))
+
+    ax[0, 0].imshow(abs(latticeFourierMask), cmap='plasma')
+    ax[0, 0].set_title('Electric field in pupil')
+
+    ax[0, 0].imshow(abs(latticeDithered_hat), cmap='plasma')
+    ax[0, 1].set_title('Fourier components of lattice intensity')
+
+    ax[0, 2].imshow(lattice_hat, cmap='plasma')
+    ax[0, 2].set_title('Fourier components of dithered lattice intensity')
+
+    ax[1, 0].imshow(lattice_efield, cmap='plasma')
+    ax[1, 0].set_title('Electric field of lattice at focal plane')
+    plt.xlim([-75, 75] + len(dispRange) / 2 + 1)
+    plt.ylim([-75, 75] + len(dispRange) / 2 + 1)
+
+    ax[1, 2].imshow(lattice, cmap='plasma')
+    ax[1, 1].set_title('Intensity of lattice')
+
+    plt.show()
+    plt.pause(0.001)
+
     demoFieldSynthesis()
