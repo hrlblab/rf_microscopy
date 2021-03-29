d2dd3a90f73d8e2318b74386efc84475b4ea0c81
yilinliu610730 <liuyilin0730@foxmail.com>
2021-03-29 14:53:49 +0800

    final

--- python/fieldSynthesis.py
+++ python/fieldSynthesis.py
@@ -1,15 +1,13 @@
 from typing import List, Union
 import numpy as np
-import pandas as pandas
-import scipy as sci
 import scipy.fftpack as ft
 import scipy.signal as sig
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 import math
 from PIL import Image
-import matplotlib.pyplot
 import pandas as pd
+from scipy.signal import convolve2d
 
 ''' Field Synthesis
 Python-based demonstration of Field Synthesis
@@ -185,7 +183,7 @@ def demoFieldSynthesis():
 
     # Manually scanning is a convolution operation
     # There are potentially boundary effects here
-    convolved = sig.fftconvolve(Fsqmod, Lsqmod, 'same')
+    convolved = sig.convolve(Fsqmod, Lsqmod, 'same')
 
     ax[1, 1].imshow(convolved, cmap='plasma')
     ax[1, 1].set_title('Convolved: $ |F(x,z)|^2 ** |L(x)\delta(z)|^2 $')
@@ -209,13 +207,40 @@ def demoFieldSynthesis():
     plt.pause(0.001)
 
 
+def shiftMatrixByK(mat, k):
+    N = 4095
+    matrix = [[]]
+    j = 0
+    if k < 0:
+        k = -k
+    else:
+        k = N - k
+    while j < N:
+        # Print elements from
+        # index k
+        for i in range(k, N):
+            matrix[j].append(mat[j][i])
+
+        # Print elements before
+        # index k
+        for i in range(0, k):
+            matrix[j].append(mat[j][i])
+
+        j = j + 1
+    return matrix
+
+
+def conv2(x, y, mode='same'):
+    return np.rot90(convolve2d(np.rot90(x, 2), np.rot90(y, 2), mode=mode), 2)
+
+
 if __name__ == "__main__":
     n = 4096
     r = 256
     w = 5
     offset = 256
     dispRange: List[Union[int, float]] = []
-    for i in range (-600, 601):
+    for i in range(-600, 601):
         dispRange.append(i + math.floor(n / 2) + 1)
     v = []
     for i in range(0, n):
@@ -230,25 +255,30 @@ if __name__ == "__main__":
             selected_columns.append(0)
     latticeFourierMask = annulus
     for c in range(0, n):
-        if selected_columns[c]:
-            latticeFourierMask[:, c] = True
+        if not selected_columns[c]:
+            latticeFourierMask[:, c] = False
+    latticeFourierMask = latticeFourierMask.astype(float)
 
     fieldSynthesisProfile = ft.fftshift(ft.ifft(ft.ifftshift(latticeFourierMask)))
-    fieldSynthesisProfile = [sum(i) for i in fieldSynthesisProfile]
-
+    df = pd.DataFrame(fieldSynthesisProfile)
+    sum_rows = df.sum(axis=1)
+    fieldSynthesisProfile = sum_rows ** 2
     lattice_efield = ft.fftshift(ft.ifft2(ft.ifftshift(latticeFourierMask)))
     lattice = abs(lattice_efield) ** 2
-    latticeLineProfile = sum(lattice, 2)
+    df1 = pd.DataFrame(lattice)
+    latticeLineProfile = df1.sum(axis=1)
     latticeLineProfile = latticeLineProfile * n
 
     lattice_hat = ft.fftshift(ft.fft2(ft.ifftshift(lattice)))
 
     period = n / offset
     if period == round(period):
-        latticeDithered = np.zeros(len(lattice));
-        for s in range(math.floor(-period / 2), math.floor(period / 2) - 1):
-            latticeDithered = latticeDithered + np.roll(lattice, s)
-        latticeDithered = latticeDithered / period
+        # latticeDithered = np.zeros((4096, 4096))
+        # for s in range(math.floor(-period / 2), math.floor(period / 2) - 1):
+        #     max = np.roll(lattice, s, axis=1)
+        #     latticeDithered = np.add(latticeDithered, max)
+        # latticeDithered = latticeDithered / period
+        latticeDithered = conv2(lattice, np.ones((1, int(period))) / period, 'same')
     else:
         for x in lattice_hat:
             for y in x:
@@ -257,27 +287,53 @@ if __name__ == "__main__":
 
     latticeDithered_hat = ft.fftshift(ft.fft2(ft.ifftshift(latticeDithered)))
 
-    # Plot 2x3
-    fig, ax = plt.subplots(2, 3, sharey=True, sharex=True, figsize=(16, 9))
+    fig = plt.figure()
+    plt.figure(figsize=(16, 9))
+    plt.gca().get_autoscale_on()
 
-    ax[0, 0].imshow(abs(latticeFourierMask), cmap='plasma')
-    ax[0, 0].set_title('Electric field in pupil')
+    ax1 = plt.subplot(231)
+    ax1.set_xlim([1600, 2500])
+    ax1.set_ylim([1600, 2500])
+    aa = abs(latticeFourierMask)
+    aa[aa > 1e-6] = 1e-6
+    ax1.imshow(latticeFourierMask, cmap='hot')
+    ax1.set_title('Electric field in pupil')
 
-    ax[0, 0].imshow(abs(latticeDithered_hat), cmap='plasma')
-    ax[0, 1].set_title('Fourier components of lattice intensity')
+    aaa = abs(lattice_hat)
+    aaa[aaa > 1e-6] = 1e-6
+    ax2 = plt.subplot(232)
+    ax2.set_xlim([1250, 2800])
+    ax2.set_ylim([1250, 2800])
+    ax2.imshow(abs(aaa), cmap='hot')
+    ax2.set_title('Fourier components of lattice intensity')
 
-    ax[0, 2].imshow(lattice_hat, cmap='plasma')
-    ax[0, 2].set_title('Fourier components of dithered lattice intensity')
+    bbb = abs(latticeDithered_hat)
+    bbb[bbb > 1e-6] = 1e-6
+    ax3 = plt.subplot(233)
+    ax3.set_xlim([1300, 2800])
+    ax3.set_ylim([1300, 2800])
+    ax3.imshow(abs(bbb), cmap='hot')
+    ax3.set_title('Fourier components of dithered lattice intensity')
 
-    ax[1, 0].imshow(lattice_efield, cmap='plasma')
-    ax[1, 0].set_title('Electric field of lattice at focal plane')
-    plt.xlim([-75, 75] + len(dispRange) / 2 + 1)
-    plt.ylim([-75, 75] + len(dispRange) / 2 + 1)
+    ax4 = plt.subplot(234)
+    ax4.imshow(np.real(lattice_efield), cmap='gray')
+    ax4.set_title('Electric field of lattice at focal plane')
+    ax4.set_xlim([1950, 2150])
+    ax4.set_ylim([1950, 2150])
 
-    ax[1, 2].imshow(lattice, cmap='plasma')
-    ax[1, 1].set_title('Intensity of lattice')
+    ax5 = plt.subplot(235)
+    ax5.imshow(lattice, cmap='hot')
+    ax5.set_title('Intensity of lattice')
+    ax5.set_xlim([1950, 2150])
+    ax5.set_ylim([1950, 2150])
+
+    ax6 = plt.subplot(236)
+    ax6.imshow(latticeDithered, cmap='hot')
+    ax6.set_title('Averaged Intensity of dithered lattice')
+    ax6.set_xlim([1950, 2150])
+    ax6.set_ylim([1950, 2150])
 
     plt.show()
     plt.pause(0.001)
 
-    demoFieldSynthesis()
+    ##demoFieldSynthesis()
